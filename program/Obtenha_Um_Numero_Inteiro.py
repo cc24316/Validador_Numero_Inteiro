9a96ac96ccea437e91c255d12157713b238deac9
Isabela Pak Bragagnolo <162496392+cc24316@users.noreply.github.com>
2026-05-06 15:33:29 -0300

    Update Obtenha_Um_Numero_Inteiro.py

--- program/Obtenha_Um_Numero_Inteiro.py
+++ program/Obtenha_Um_Numero_Inteiro.py
@@ -1,1 +1,28 @@
+def obtenha_um_numero_inteiro(frase, min=None, max=None, proibidos=None):
+    digitou_corretamente = False
+    
+    while not digitou_corretamente:
+        try:
+            numero = int(input(frase))
+        except ValueError:
+            print("Um número inteiro devia ter sido digitado; tente novamente!")
+        else:
+            if min != None and numero < min:
+                print("Um valor de", min, "para cima deveria ter sido digitado, tente novamente!")
+            elif max != None and numero > max:
+                print("Um valor de", max, "para baixo deveria ter sido digitado, tente novamente!")
+            elif proibidos != None and numero in proibidos:
+                print("O(s) valor(es)", proibidos, "são inaceitáveis, tente novamente!")
+            else:
+                digitou_corretamente = True
+                
+    return numero
 
+numero = obtenha_um_numero_inteiro(
+    "Digite um número: ",
+    min=None,
+    max=100,
+    proibidos=[10, 20, 30, 40, 50, 60, 70, 80, 90]
+)
+
+print("Número válido digitado:", numero)
